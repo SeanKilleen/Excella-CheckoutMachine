141872032b843a3f3d1fa62e2a1eedf841f0a812
Sean Killeen <SeanKilleen@gmail.com>
2017-01-18 19:34:55 -0500

    Add non-compiling test

---
+++ src/test/Tests.Unit.Excella.CheckoutMachine/CheckoutMachineTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests.Unit.Excella.CheckoutMachine
+{
+    public class CheckoutMachineTests
+    {
+        [Test]
+        public void GetTotal_WithNoItemsScanned_Returns0()
+        {
+            var sut = new CheckoutMachine();
+
+            var result = sut.GetTotal();
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
