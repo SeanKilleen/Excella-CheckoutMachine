d4d40f341c589c5cff97a5bfff6f981af8d3e67b
Sean Killeen <SeanKilleen@gmail.com>
2017-01-18 19:37:42 -0500

    Add class with simplest passing test

---
+++ src/app/Excella.CheckoutMachine/SelfCheckoutMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excella.CheckoutMachine
+{
+    public class SelfCheckoutMachine
+    {
+        public int GetTotal()
+        {
+            return 0;
+        }
+    }
+}
--- src/test/Tests.Unit.Excella.CheckoutMachine/SelfCheckoutMachineTests.cs
+++ src/test/Tests.Unit.Excella.CheckoutMachine/SelfCheckoutMachineTests.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-
+using Excella.CheckoutMachine;
 namespace Tests.Unit.Excella.CheckoutMachine
 {
-    public class CheckoutMachineTests
+    public class SelfCheckoutMachineTests
     {
         [Test]
         public void GetTotal_WithNoItemsScanned_Returns0()
         {
-            var sut = new CheckoutMachine();
+            var sut = new SelfCheckoutMachine();
 
             var result = sut.GetTotal();
 
