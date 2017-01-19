71d4087ceab83f4d78007be8480f5ba9c1de4ea3
Sean Killeen <SeanKilleen@gmail.com>
2017-01-18 20:05:07 -0500

    Create a class to hold SKU numbers as constants

---
+++ src/app/Excella.CheckoutMachine/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excella.CheckoutMachine
+{
+    public class Constants
+    {
+        public class SkuNumbers
+        {
+        }
+    }
+}
