4789b3e184a203bffbf2476a44f6dd8bbcbb2331
Sean Killeen <SeanKilleen@gmail.com>
2017-01-18 20:37:08 -0500

    Remove previous individual tests in favor of test case

--- src/test/Tests.Unit.Excella.CheckoutMachine/SelfCheckoutMachineTests.cs
+++ src/test/Tests.Unit.Excella.CheckoutMachine/SelfCheckoutMachineTests.cs
@@ -58,55 +58,6 @@ namespace Tests.Unit.Excella.CheckoutMachine
         }
 
         [Test]
-        public void Scan_WithTwoBagsOfChips_ExpectTotalOf400()
-        {
-            var sut = new SelfCheckoutMachine();
-
-            sut.Scan(Constants.SkuNumbers.CHIPS);
-            sut.Scan(Constants.SkuNumbers.CHIPS);
-
-            var result = sut.GetTotal();
-
-            Assert.That(result, Is.EqualTo(400));
-        }
-
-        [Test]
-        public void Scan_WithTwoSalsas_ExpectTotalOf200()
-        {
-            var sut = new SelfCheckoutMachine();
-
-            sut.Scan(Constants.SkuNumbers.SALSA);
-            sut.Scan(Constants.SkuNumbers.SALSA);
-
-            var result = sut.GetTotal();
-
-            Assert.That(result, Is.EqualTo(200));
-        }
-        [Test]
-        public void Scan_WithTwoWines_ExpectTotalOf2000()
-        {
-            var sut = new SelfCheckoutMachine();
-
-            sut.Scan(Constants.SkuNumbers.WINE);
-            sut.Scan(Constants.SkuNumbers.WINE);
-
-            var result = sut.GetTotal();
-
-            Assert.That(result, Is.EqualTo(2000));
-        }
-        [Test]
-        public void Scan_WithTwoCigarettes_ExpectTotalOf1000()
-        {
-            var sut = new SelfCheckoutMachine();
-
-            sut.Scan(Constants.SkuNumbers.CIGARETTES);
-            sut.Scan(Constants.SkuNumbers.CIGARETTES);
-
-            var result = sut.GetTotal();
-
-            Assert.That(result, Is.EqualTo(1000));
-        }
-        [Test]
         public void Scan_WithOneOfEachItem_ExpectTotalOf1800()
         {
             var sut = new SelfCheckoutMachine();
